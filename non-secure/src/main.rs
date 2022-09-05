f4eaef02722578a2f3b27e437703955a0e4d7989
Dion Dokter <dion@tweedegolf.com>
2022-09-05 14:21:46 +0200

    Removed adler function

--- non-secure/src/main.rs
+++ non-secure/src/main.rs
@@ -30,25 +30,6 @@ pub extern "C" fn read_thing() -> u32 {
     unsafe { THING }
 }
 
-const fn adler32(string: &str) -> u32 {
-    const MOD_ADLER: u32 = 65521;
-
-    let data = string.as_bytes();
-
-    let mut a = 1;
-    let mut b = 0;
-
-    let mut index = 0;
-
-    while index < data.len() {
-        a = (a + data[index] as u32) % MOD_ADLER;
-        b = (b + a) % MOD_ADLER;
-        index += 1;
-    }
-
-    (b << 16) | a
-}
-
 /// Called when our code panics.
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
