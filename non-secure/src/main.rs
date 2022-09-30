561d40d3b10faa9bb080ffda5eb67cda42ac2f58
Dion Dokter <diondokter@gmail.com>
2022-09-30 16:17:38 +0200

    Back to bindings

--- non-secure/src/main.rs
+++ non-secure/src/main.rs
@@ -6,52 +6,7 @@ use trustzone_m_macros::secure_callable;
 mod other_private_thing;
 pub mod other_public_thing;
 
-// include!(concat!(env!("OUT_DIR"), "/trustzone_bindings.rs"));
-
-pub mod trustzone_bindings {
-    #[inline(never)]
-    pub extern "C" fn return_5() -> u32 {
-        const HASH: u32 = 3146409456u32;
-        let fn_ptr = unsafe { super::find_nsc_vector::<extern "C" fn() -> u32>(HASH).unwrap() };
-        fn_ptr()
-    }
-}
-#[allow(dead_code)]
-#[inline(never)]
-unsafe fn find_ns_vector<F>(name_hash: u32) -> Option<F> {
-    extern "C" {
-        static _NS_VECTORS: u32;
-    }
-    let mut ns_vectors_ptr = &_NS_VECTORS as *const u32 as *const (u32, u32);
-    loop {
-        let (vector, vector_hash) = *ns_vectors_ptr;
-        if vector == 0 && vector_hash == 0 {
-            return None;
-        }
-        if vector_hash == name_hash {
-            return Some(core::mem::transmute_copy(&vector));
-        }
-        ns_vectors_ptr = ns_vectors_ptr.offset(1);
-    }
-}
-#[allow(dead_code)]
-#[inline(never)]
-unsafe fn find_nsc_vector<F>(name_hash: u32) -> Option<F> {
-    extern "C" {
-        static _NSC_VECTORS: u32;
-    }
-    let mut nsc_vectors_ptr = &_NSC_VECTORS as *const u32 as *const (u32, u32);
-    loop {
-        let (vector, vector_hash) = *nsc_vectors_ptr;
-        if vector == 0 && vector_hash == 0 {
-            return None;
-        }
-        if vector_hash == name_hash {
-            return Some(core::mem::transmute_copy(&vector));
-        }
-        nsc_vectors_ptr = nsc_vectors_ptr.offset(1);
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/trustzone_bindings.rs"));
 
 static mut THING: u32 = 0;
 
